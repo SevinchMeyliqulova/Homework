99f8c6e837708c462d463b0392d907e9c9b5feee
SevinchMeyliqulova <135237116+SevinchMeyliqulova@users.noreply.github.com>
2023-06-01 18:15:23 +0500

    Add files via upload

---
+++ sss/js/script.js
@@ -0,0 +1,145 @@
+          // arr uyga vazifa
+// let arr = [12, 3, 5, 34, 7]
+// let sum = 0
+// for(let i = 0; i < arr.length; i++){
+//    sum += arr[i]
+// }
+// console.log(sum)
+
+
+
+
+// let arr = [12, 3, -5, -34, 7]
+// let sum = 0
+// for(let i = 0; i < arr.length; i++){
+//     if(arr.length[i] < 0){
+//         sum += arr[i] * -1
+//     }else if(arr.length = 0 && arr.length[i] > 0){
+//         return "0"
+//     }
+// }
+// return sum
+
+
+
+// let arr = [12, 3, 3, 34, -5, -34, 7]
+// let result = []
+// for(let i = 0; i < arr.length; i++){
+//     if(arr.length[i] == arr.length[i]){
+//         result = arr.length[i]
+//     }
+// }
+// console.log(result)
+
+
+
+        //    funktsiya uyga vazifa
+// let arr = [12, 3, 3, 34, 23, 7]
+// function calcAverage(numbers){
+//     let sum = 0
+//     for(let i = 0; i < arr.length; i++){
+//        sum += arr[i]   
+//     }
+//     console.log(sum/arr.length)
+// }
+// calcAverage(arr)
+
+
+
+// function isPalindrome(x) {
+//     let result = x.toLowerCase().split('').reverse()
+//     if(x.toLowerCase() == result.join('')){
+//       return true
+//     }else{
+//       return false
+//     }
+//   }
+// console.log(isPalindrome(abba))  
+
+
+
+
+
+          // number uyga vazifa
+// function floor(num) {
+//     let result;
+//     if(num > 0){
+//        result = num - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         result = num -1 + ((num * 10) % 10) / 10 
+//     }
+//         console.log(result)
+// }
+// floor(12.4)
+
+
+
+// function ceil(num) {
+//     let result;
+//     if(num > 0){
+//        result = num + 1 - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         result = num - ((num * 10) % 10) / 10 
+//     }
+//     console.log(result)
+// }
+// ceil(12.4)
+
+
+
+// function round(num) {
+//     let result;
+//     if(((num * 10) % 10) > 5){
+//         result = num + 1 - ((num * 10) % 10) / 10 
+//     }else if(((num * 10) % 10) < 5){
+//         result = num - ((num * 10) % 10) / 10 
+//     }
+//     console.log(result)
+// }
+// round(12,6)
+
+
+
+// function trunc(num) {
+//     let result 
+//     if(num > 0){
+//         result = num - ((num * 10) % 10) / 10
+//     }else if(num < 0){
+//         result = (num - ((num * 10) % 10) / 10) * -1
+//     }
+//     console.log(result)
+// }
+// trunc(-14,5)
+
+
+// function abc(num) {
+//     let result
+//     if(num > 0){
+//         result = num
+//     }else if(num < 0){
+//         result = num * -1
+//     }
+//     console.log(result)
+// }
+// abc(12.9)
+
+
+
+
+
+// function pow(x, y) {
+//     let result = x ** y
+//     console.log(result)
+// }
+// pow(16, 1/2)
+
+
+
+// function sqrt(num) {
+//   // let result
+//   if(num > 0){
+//     let  nums = x * x
+    
+//   }
+// }
+// sqrt(16)
